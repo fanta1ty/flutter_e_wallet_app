afc7249a4709615fb2940d18abe922697c2a06e2
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-02 23:26:18 +0700

    Create submited_to_friend_state.dart

---
+++ e_wallet/lib/screen/submited_to_friend/submited_to_friend_state.dart
@@ -0,0 +1,35 @@
+part of 'submited_to_friend_cubit.dart';
+
+class SubmitedToFriendState {
+  SubmitedToFriendState.init();
+
+//<editor-fold desc="Data Methods">
+  const SubmitedToFriendState();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is SubmitedToFriendState && runtimeType == other.runtimeType);
+
+  @override
+  int get hashCode => 0;
+
+  @override
+  String toString() {
+    return 'SubmitedToFriendState{' + '}';
+  }
+
+  SubmitedToFriendState copyWith() {
+    return SubmitedToFriendState();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {};
+  }
+
+  factory SubmitedToFriendState.fromMap(Map<String, dynamic> map) {
+    return SubmitedToFriendState();
+  }
+
+//</editor-fold>
+}
