3e211e6430e9e3416129f005eced0e0d6fcd2aae
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-02 23:42:37 +0700

    Update transfer_to_contact_cubit.dart

--- e_wallet/lib/screen/transfer_to_contact/transfer_to_contact_cubit.dart
+++ e_wallet/lib/screen/transfer_to_contact/transfer_to_contact_cubit.dart
@@ -1,28 +1,37 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../constant/load_status.dart';
+
 part 'transfer_to_contact_state.dart';
 
 class TransferToContactCubit extends Cubit<TransferToContactState> {
   TransferToContactCubit()
       : super(
-          TransferToContactState.init(
-            false,
-            false,
-            false,
-          ),
+          TransferToContactState.init(),
         );
 
   Future<void> setButtonEnabled(
     bool isEnabled,
   ) async {
-    emit(state.copyWith(isButtonEnabled: isEnabled));
+    emit(
+      state.copyWith(isButtonEnabled: isEnabled),
+    );
   }
 
   Future<void> checkIsProceedToTransfer(
     bool isProceedToTransfer,
   ) async {
-    emit(state.copyWith(isProceedToTransfer: isProceedToTransfer));
+    emit(
+      state.copyWith(isProceedToTransfer: isProceedToTransfer),
+    );
+    if (isProceedToTransfer) {
+      emit(
+        state.copyWith(
+          loadStatus: LoadStatus.Loading,
+        ),
+      );
+    }
   }
 
   Future<void> saveAmount(
@@ -34,6 +43,7 @@ class TransferToContactCubit extends Cubit<TransferToContactState> {
           'transactions',
         )
         .doc();
+
     await doc.set(
       {
         'amount': amount,
@@ -42,6 +52,14 @@ class TransferToContactCubit extends Cubit<TransferToContactState> {
       },
     );
 
-    emit(state.copyWith(isTransferSuccess: true));
+    emit(
+      state.copyWith(
+        loadStatus: LoadStatus.Done,
+      ),
+    );
+
+    emit(
+      state.copyWith(isTransferSuccess: true),
+    );
   }
 }
