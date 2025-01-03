af3cf0956f645b1ad4b55fc8262879b5f42ef91a
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 16:40:51 +0700

    Update transfer_state.dart

--- e_wallet/lib/screen/transfer/transfer_state.dart
+++ e_wallet/lib/screen/transfer/transfer_state.dart
@@ -1,34 +1,72 @@
 part of 'transfer_cubit.dart';
 
 class TransferState {
-  TransferState.init();
+  final LoadStatus loadStatus;
+  List<TransactionResponse> responses;
+
+  final String transactionImage;
+
+  TransferState.init({
+    this.loadStatus = LoadStatus.Init,
+    this.responses = const [],
+    this.transactionImage = '',
+  });
 
 //<editor-fold desc="Data Methods">
-  const TransferState();
+  TransferState({
+    required this.loadStatus,
+    required this.responses,
+    required this.transactionImage,
+  });
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is TransferState && runtimeType == other.runtimeType);
+      (other is TransferState &&
+          runtimeType == other.runtimeType &&
+          loadStatus == other.loadStatus &&
+          responses == other.responses &&
+          transactionImage == other.transactionImage);
 
   @override
-  int get hashCode => 0;
+  int get hashCode =>
+      loadStatus.hashCode ^ responses.hashCode ^ transactionImage.hashCode;
 
   @override
   String toString() {
-    return 'TransferState{' + '}';
+    return 'TransferState{' +
+        ' loadStatus: $loadStatus,' +
+        ' responses: $responses,' +
+        ' transactionImage: $transactionImage,' +
+        '}';
   }
 
-  TransferState copyWith() {
-    return TransferState();
+  TransferState copyWith({
+    LoadStatus? loadStatus,
+    List<TransactionResponse>? responses,
+    String? transactionImage,
+  }) {
+    return TransferState(
+      loadStatus: loadStatus ?? this.loadStatus,
+      responses: responses ?? this.responses,
+      transactionImage: transactionImage ?? this.transactionImage,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    return {};
+    return {
+      'loadStatus': this.loadStatus,
+      'responses': this.responses,
+      'transactionImage': this.transactionImage,
+    };
   }
 
   factory TransferState.fromMap(Map<String, dynamic> map) {
-    return TransferState();
+    return TransferState(
+      loadStatus: map['loadStatus'] as LoadStatus,
+      responses: map['responses'] as List<TransactionResponse>,
+      transactionImage: map['transactionImage'] as String,
+    );
   }
 
 //</editor-fold>
