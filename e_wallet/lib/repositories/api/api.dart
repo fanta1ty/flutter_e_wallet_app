a4734943851a8b105844c9f6abc5d2aef58460e6
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 16:40:44 +0700

    Update api.dart

--- e_wallet/lib/repositories/api/api.dart
+++ e_wallet/lib/repositories/api/api.dart
@@ -3,6 +3,7 @@ import 'package:e_wallet/models/request/signup_request.dart';
 import 'package:e_wallet/models/request/transfer_request.dart';
 import 'package:e_wallet/models/response/login_response.dart';
 import 'package:e_wallet/models/response/signup_response.dart';
+import 'package:e_wallet/models/response/transaction_response.dart';
 
 abstract class Api {
   Future<LoginResponse> login(
@@ -15,5 +16,9 @@ abstract class Api {
 
   Future<void> transfer(
     TransferRequest request,
+  );
+
+  Future<List<TransactionResponse>> fetchTransactions(
+    String collectionPath,
   );
 }
