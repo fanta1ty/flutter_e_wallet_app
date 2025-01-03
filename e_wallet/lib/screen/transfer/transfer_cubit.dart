890775e011d0b76a91302e33a89aff0df053be14
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 16:40:49 +0700

    Update transfer_cubit.dart

--- e_wallet/lib/screen/transfer/transfer_cubit.dart
+++ e_wallet/lib/screen/transfer/transfer_cubit.dart
@@ -1,7 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:e_wallet/models/response/transaction_response.dart';
+
+import '../../constant/load_status.dart';
+import '../../repositories/api/api.dart';
 
 part 'transfer_state.dart';
 
 class TransferCubit extends Cubit<TransferState> {
-  TransferCubit() : super(TransferState.init());
+  final Api api;
+
+  TransferCubit(this.api) : super(TransferState.init());
+
+  Future<void> fetchTransactions() async {
+    emit(state.copyWith(loadStatus: LoadStatus.Loading));
+    var result = await api.fetchTransactions('transactions');
+    emit(state.copyWith(
+      loadStatus: LoadStatus.Done,
+      responses: result,
+    ));
+  }
 }
