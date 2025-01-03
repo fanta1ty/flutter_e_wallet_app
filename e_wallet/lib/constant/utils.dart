ee746593840fdbaf7090e991ad3e29810499e22c
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 17:08:11 +0700

    Update utils.dart

--- e_wallet/lib/constant/utils.dart
+++ e_wallet/lib/constant/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 String formattedPhone(String phone) {
   final cleanPhone = phone.replaceAll(RegExp(r'[^0-9]'), '');
   if (cleanPhone.length == 10) {
@@ -6,4 +8,58 @@ String formattedPhone(String phone) {
     return '+1 (${cleanPhone.substring(1, 4)}) ${cleanPhone.substring(4, 7)}-${cleanPhone.substring(7)}';
   }
   return phone;
+}
+
+String generateVietnameseName() {
+  final Random random = Random();
+
+  // Common Vietnamese last names
+  List<String> lastNames = [
+    'Nguyễn',
+    'Trần',
+    'Lê',
+    'Phạm',
+    'Huỳnh',
+    'Hoàng',
+    'Vũ',
+    'Đặng',
+    'Bùi',
+    'Đỗ'
+  ];
+
+  // Common Vietnamese middle names
+  List<String> middleNames = [
+    'Thị',
+    'Văn',
+    'Thanh',
+    'Minh',
+    'Đức',
+    'Quang',
+    'Bảo',
+    'Ngọc',
+    'Tuấn',
+    'Hồng'
+  ];
+
+  // Common Vietnamese first names
+  List<String> firstNames = [
+    'An',
+    'Bình',
+    'Châu',
+    'Duy',
+    'Giang',
+    'Hạnh',
+    'Khánh',
+    'Lâm',
+    'Nhi',
+    'Quyên'
+  ];
+
+  // Randomly select each part of the name
+  String lastName = lastNames[random.nextInt(lastNames.length)];
+  String middleName = middleNames[random.nextInt(middleNames.length)];
+  String firstName = firstNames[random.nextInt(firstNames.length)];
+
+  // Assemble the full name
+  return '$lastName $middleName $firstName';
 }
