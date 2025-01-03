0f56650c9c5f13e00067d66af7e845f79c2d2d21
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 14:01:24 +0700

    Update transfer_using_bank_cubit.dart

--- e_wallet/lib/screen/transfer_using_bank/transfer_using_bank_cubit.dart
+++ e_wallet/lib/screen/transfer_using_bank/transfer_using_bank_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../constant/load_status.dart';
+import '../../models/request/transfer_request.dart';
 import '../../repositories/api/api.dart';
 
 part 'transfer_using_bank_state.dart';
@@ -27,14 +28,22 @@ class TransferUsingBankCubit extends Cubit<TransferUsingBankState> {
     String phone,
     String date,
     String bankDate,
+    String bankCode,
+    String to,
+    String from,
   ) async {
     emit(state.copyWith(loadStatus: LoadStatus.Loading));
     await api.transfer(
-      amount,
-      note,
-      phone,
-      date,
-      bankDate,
+      TransferRequest(
+        amount: amount,
+        note: note,
+        phone: phone,
+        date: date,
+        bankDate: bankDate,
+        bankCode: bankCode,
+        to: to,
+        from: from,
+      ),
     );
     emit(state.copyWith(loadStatus: LoadStatus.Done));
     emit(state.copyWith(isTransferSuccess: true));
