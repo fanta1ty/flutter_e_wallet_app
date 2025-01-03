63c3c464ec088532df56ea2afce7c127ca8bc45f
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 16:40:47 +0700

    Update transfer.dart

--- e_wallet/lib/screen/transfer/transfer.dart
+++ e_wallet/lib/screen/transfer/transfer.dart
@@ -1,10 +1,16 @@
+import 'dart:math';
+
 import 'package:e_wallet/constant/colours.dart';
+import 'package:e_wallet/repositories/api/api.dart';
 import 'package:e_wallet/screen/nabbar/nabbar.dart';
 import 'package:e_wallet/screen/transfer/transfer_cubit.dart';
 import 'package:e_wallet/screen/transfer_to_bank/transfer_to_bank.dart';
 import 'package:e_wallet/screen/transfer_to_friend/transfer_to_friend.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+
+import '../../constant/load_status.dart';
 
 class Transfer extends StatelessWidget {
   const Transfer({super.key});
@@ -12,42 +18,22 @@ class Transfer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => TransferCubit(),
+      create: (context) =>
+          TransferCubit(context.read<Api>())..fetchTransactions(),
       child: _TransferPage(),
     );
   }
 }
 
 class _TransferPage extends StatelessWidget {
-  final List<Map<String, dynamic>> transactions = [
-    {
-      'name': 'VCB - Thinh Nguyen',
-      'date': 'Dec 31, 2024 · 21:54',
-      'amount': 745000,
-      'image': 'assets/image/Frame 3.png',
-    },
-    {
-      'name': 'Techcombank - Thinh Nguyen',
-      'date': 'Jan 1, 2025 · 04:18',
-      'amount': 450000,
-      'image': 'assets/image/Frame 3 (1).png',
-    },
-    {
-      'name': 'VietinBank - Thinh Nguyen',
-      'date': 'Jan 2, 2025 · 04:18',
-      'amount': 450000,
-      'image': 'assets/image/Frame 3 (2).png',
-    },
-  ];
+  _TransferPage();
 
-  _TransferPage({super.key});
-
-  String formatAmount(int amount) {
+  String formatAmount(String amount) {
     // Format the amount to Indonesian Rupiah format
-    return '\$ ${amount.toString().replaceAllMapped(
-          RegExp(r'(\d)(?=(\d{3})+$)'),
-          (Match m) => '${m[1]}.',
-        )}';
+    return '\$ ${amount.replaceAllMapped(
+      RegExp(r'(\d)(?=(\d{3})+$)'),
+      (Match m) => '${m[1]}.',
+    )}';
   }
 
   @override
@@ -89,150 +75,185 @@ class _TransferPage extends StatelessWidget {
       ),
       body: BlocConsumer<TransferCubit, TransferState>(
         builder: (context, state) {
-          return SingleChildScrollView(
-            child: Container(
-              width: _screenWidth,
-              height: _screenHeight,
-              color: btntxt,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    topLeft: Radius.circular(30),
-                  ),
-                ),
-                margin: const EdgeInsets.only(
-                  top: 30,
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 5,
-                            top: 30,
+          final transactions = context.watch<TransferCubit>().state.responses;
+
+          return state.loadStatus == LoadStatus.Loading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : SingleChildScrollView(
+                  child: Container(
+                    width: _screenWidth,
+                    height: _screenHeight,
+                    color: btntxt,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          topLeft: Radius.circular(30),
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(
+                        top: 30,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 5,
+                                  top: 30,
+                                ),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            TransferToFriend(),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    width: 150,
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFFf9f5ff),
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                          child: Icon(
+                                            Icons.account_balance,
+                                            color: btntxt,
+                                            size: 50,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Transfer to Friends',
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10,
+                                  top: 30,
+                                ),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => TransferToBank(),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    width: 150,
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFFf9f5ff),
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                          child: Icon(
+                                            Icons.account_balance,
+                                            color: btntxt,
+                                            size: 50,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Transfer to Bank',
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TransferToFriend(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 30, left: 10),
+                            child: Text(
+                              "Latest Transfer",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: transactions.length,
+                            itemBuilder: (context, index) {
+                              final transaction = transactions[index];
+                              String title = '';
+                              String imagePath = '';
+                              String transactionDate = '';
+
+                              if (transaction.bankDate != null &&
+                                  transaction.bankDate!.isNotEmpty) {
+                                title =
+                                    '${transaction.bankCode} - ${transaction.to}';
+                                imagePath =
+                                    fetchBankImageWith(transaction.bankCode);
+                                transactionDate = transaction.bankDate!;
+                              } else {
+                                title = transaction.phone;
+                                transactionDate = transaction.date!;
+                                imagePath =
+                                    'assets/image/avatar_${Random().nextInt(4) + 1}.png';
+                              }
+
+                              final parsedDate =
+                                  DateTime.parse(transactionDate);
+                              final formatedTransactionDate =
+                                  DateFormat('MMMM dd, yyyy - hh:mm a')
+                                      .format(parsedDate);
+
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: AssetImage(imagePath),
+                                  radius: 25,
+                                ),
+                                title: Text(
+                                  title,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                  formatedTransactionDate,
+                                ),
+                                trailing: Text(
+                                  formatAmount(transaction.amount),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
                                 ),
                               );
                             },
-                            child: Container(
-                              width: 150,
-                              height: 100,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFf9f5ff),
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Center(
-                                    child: Icon(
-                                      Icons.account_balance,
-                                      color: btntxt,
-                                      size: 50,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    'Transfer to Friends',
-                                  )
-                                ],
-                              ),
-                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 10,
-                            top: 30,
-                          ),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TransferToBank(),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 100,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFf9f5ff),
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Center(
-                                    child: Icon(
-                                      Icons.account_balance,
-                                      color: btntxt,
-                                      size: 50,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    'Transfer to Bank',
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, left: 10),
-                      child: Text(
-                        "Latest Transfer",
-                        style: TextStyle(fontSize: 18),
+                        ],
                       ),
                     ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: transactions.length,
-                      itemBuilder: (context, index) {
-                        final transaction = transactions[index];
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: AssetImage(transaction['image']),
-                            radius: 25,
-                          ),
-                          title: Text(
-                            transaction['name'],
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text(transaction['date']),
-                          trailing: Text(
-                            formatAmount(transaction['amount']),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+                  ),
+                );
         },
         listener: (context, state) {},
       ),
