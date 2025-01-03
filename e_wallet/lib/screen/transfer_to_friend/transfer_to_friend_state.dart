7d1e36ca6dc25882c46ee1114f38f54205f70b24
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 23:02:35 +0700

    Update transfer_to_friend_state.dart

--- e_wallet/lib/screen/transfer_to_friend/transfer_to_friend_state.dart
+++ e_wallet/lib/screen/transfer_to_friend/transfer_to_friend_state.dart
@@ -6,16 +6,33 @@ class TransferToFriendState {
   final bool isButtonEnabled;
   final bool isTransferSuccess;
 
-  TransferToFriendState.init(
-      {this.loadStatus = LoadStatus.Init,
-      this.isButtonEnabled = false,
-      this.isTransferSuccess = false});
+  final String phone;
+  final String notes;
+  final String amount;
+  final String to;
+  final String date;
+
+  TransferToFriendState.init({
+    this.loadStatus = LoadStatus.Init,
+    this.isButtonEnabled = false,
+    this.isTransferSuccess = false,
+    this.phone = '',
+    this.notes = '',
+    this.amount = '',
+    this.to = '',
+    this.date = '',
+  });
 
 //<editor-fold desc="Data Methods">
   const TransferToFriendState({
     required this.loadStatus,
     required this.isButtonEnabled,
     required this.isTransferSuccess,
+    required this.phone,
+    required this.notes,
+    required this.amount,
+    required this.to,
+    required this.date,
   });
 
   @override
@@ -25,13 +42,23 @@ class TransferToFriendState {
           runtimeType == other.runtimeType &&
           loadStatus == other.loadStatus &&
           isButtonEnabled == other.isButtonEnabled &&
-          isTransferSuccess == other.isTransferSuccess);
+          isTransferSuccess == other.isTransferSuccess &&
+          phone == other.phone &&
+          notes == other.notes &&
+          amount == other.amount &&
+          to == other.to &&
+          date == other.date);
 
   @override
   int get hashCode =>
       loadStatus.hashCode ^
       isButtonEnabled.hashCode ^
-      isTransferSuccess.hashCode;
+      isTransferSuccess.hashCode ^
+      phone.hashCode ^
+      notes.hashCode ^
+      amount.hashCode ^
+      to.hashCode ^
+      date.hashCode;
 
   @override
   String toString() {
@@ -39,6 +66,11 @@ class TransferToFriendState {
         ' loadStatus: $loadStatus,' +
         ' isButtonEnabled: $isButtonEnabled,' +
         ' isTransferSuccess: $isTransferSuccess,' +
+        ' phone: $phone,' +
+        ' notes: $notes,' +
+        ' amount: $amount,' +
+        ' to: $to,' +
+        ' date: $date,' +
         '}';
   }
 
@@ -46,11 +78,21 @@ class TransferToFriendState {
     LoadStatus? loadStatus,
     bool? isButtonEnabled,
     bool? isTransferSuccess,
+    String? phone,
+    String? notes,
+    String? amount,
+    String? to,
+    String? date,
   }) {
     return TransferToFriendState(
       loadStatus: loadStatus ?? this.loadStatus,
       isButtonEnabled: isButtonEnabled ?? this.isButtonEnabled,
       isTransferSuccess: isTransferSuccess ?? this.isTransferSuccess,
+      phone: phone ?? this.phone,
+      notes: notes ?? this.notes,
+      amount: amount ?? this.amount,
+      to: to ?? this.to,
+      date: date ?? this.date,
     );
   }
 
@@ -59,6 +101,11 @@ class TransferToFriendState {
       'loadStatus': this.loadStatus,
       'isButtonEnabled': this.isButtonEnabled,
       'isTransferSuccess': this.isTransferSuccess,
+      'phone': this.phone,
+      'notes': this.notes,
+      'amount': this.amount,
+      'to': this.to,
+      'date': this.date,
     };
   }
 
@@ -67,6 +114,11 @@ class TransferToFriendState {
       loadStatus: map['loadStatus'] as LoadStatus,
       isButtonEnabled: map['isButtonEnabled'] as bool,
       isTransferSuccess: map['isTransferSuccess'] as bool,
+      phone: map['phone'] as String,
+      notes: map['notes'] as String,
+      amount: map['amount'] as String,
+      to: map['to'] as String,
+      date: map['date'] as String,
     );
   }
 
