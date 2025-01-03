6f5f1eaf7e720ddc3cd14925fb2831a866a72129
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-03 14:01:20 +0700

    Update transfer_to_friend_cubit.dart

--- e_wallet/lib/screen/transfer_to_friend/transfer_to_friend_cubit.dart
+++ e_wallet/lib/screen/transfer_to_friend/transfer_to_friend_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../constant/load_status.dart';
+import '../../models/request/transfer_request.dart';
 import '../../repositories/api/api.dart';
 
 part 'transfer_to_friend_state.dart';
@@ -23,14 +24,22 @@ class TransferToFriendCubit extends Cubit<TransferToFriendState> {
     String phone,
     String date,
     String bankDate,
+    String bankCode,
+    String to,
+    String from,
   ) async {
     emit(state.copyWith(loadStatus: LoadStatus.Loading));
     await api.transfer(
-      amount,
-      note,
-      phone,
-      date,
-      bankDate,
+      TransferRequest(
+        amount: amount,
+        note: note,
+        phone: phone,
+        date: date,
+        bankDate: bankDate,
+        bankCode: bankCode,
+        to: to,
+        from: from,
+      ),
     );
     emit(state.copyWith(loadStatus: LoadStatus.Done));
     emit(state.copyWith(isTransferSuccess: true));
