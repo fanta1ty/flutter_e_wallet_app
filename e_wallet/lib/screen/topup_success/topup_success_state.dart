e4b5e63d09a700c276124f9d9df4d1fc3e001eb9
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-07 00:43:59 +0700

    Create topup_success_state.dart

---
+++ e_wallet/lib/screen/topup_success/topup_success_state.dart
@@ -0,0 +1,35 @@
+part of 'topup_success_cubit.dart';
+
+class TopupSuccessState {
+  TopupSuccessState.init();
+
+//<editor-fold desc="Data Methods">
+  const TopupSuccessState();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is TopupSuccessState && runtimeType == other.runtimeType);
+
+  @override
+  int get hashCode => 0;
+
+  @override
+  String toString() {
+    return 'TopupSuccessState{' + '}';
+  }
+
+  TopupSuccessState copyWith() {
+    return TopupSuccessState();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {};
+  }
+
+  factory TopupSuccessState.fromMap(Map<String, dynamic> map) {
+    return TopupSuccessState();
+  }
+
+//</editor-fold>
+}
