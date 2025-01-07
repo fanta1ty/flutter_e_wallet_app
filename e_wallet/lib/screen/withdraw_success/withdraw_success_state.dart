8ff9541e2849292a49048b756afe0116de38684c
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-07 22:06:54 +0700

    Create withdraw_success_state.dart

---
+++ e_wallet/lib/screen/withdraw_success/withdraw_success_state.dart
@@ -0,0 +1,35 @@
+part of 'withdraw_success_cubit.dart';
+
+class WithdrawSuccessState {
+  WithdrawSuccessState.init();
+
+//<editor-fold desc="Data Methods">
+  const WithdrawSuccessState();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is WithdrawSuccessState && runtimeType == other.runtimeType);
+
+  @override
+  int get hashCode => 0;
+
+  @override
+  String toString() {
+    return 'WithdrawSuccessState{' + '}';
+  }
+
+  WithdrawSuccessState copyWith() {
+    return WithdrawSuccessState();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {};
+  }
+
+  factory WithdrawSuccessState.fromMap(Map<String, dynamic> map) {
+    return WithdrawSuccessState();
+  }
+
+//</editor-fold>
+}
