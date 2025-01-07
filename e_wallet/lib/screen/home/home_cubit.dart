fcd4b414f7405d28eeb3ccbab4e9a129f6a4d378
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-07 23:14:34 +0700

    Update home_cubit.dart

--- e_wallet/lib/screen/home/home_cubit.dart
+++ e_wallet/lib/screen/home/home_cubit.dart
@@ -1,7 +1,44 @@
 import 'package:bloc/bloc.dart';
 
+import '../../constant/load_status.dart';
+import '../../models/response/transaction_response.dart';
+import '../../repositories/api/api.dart';
+
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeState.init());
+  final Api api;
+
+  HomeCubit(this.api) : super(HomeState.init());
+
+  Future<void> fetchTransactions() async {
+    emit(state.copyWith(loadStatus: LoadStatus.Loading));
+    var result = await api.fetchTransactions('transactions');
+
+    final topUpCount = result
+        .where(
+          (transaction) => transaction.type == 'top_up',
+        )
+        .length;
+    final transferCount = result
+        .where(
+          (transaction) => transaction.type == 'transfer',
+        )
+        .length;
+    final withdrawCount = result
+        .where(
+          (transaction) => transaction.type == 'withdraw',
+        )
+        .length;
+
+    emit(
+      state.copyWith(
+        loadStatus: LoadStatus.Done,
+        responses: result,
+        topUpCount: topUpCount,
+        transferCount: transferCount,
+        withdrawCount: withdrawCount,
+      ),
+    );
+  }
 }
