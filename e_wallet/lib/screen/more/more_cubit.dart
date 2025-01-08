7c0da7c7d676c9aac7c463e178c57ee3c27f6897
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 08:44:40 +0700

    Update more_cubit.dart

--- e_wallet/lib/screen/more/more_cubit.dart
+++ e_wallet/lib/screen/more/more_cubit.dart
@@ -5,14 +5,6 @@ part 'more_state.dart';
 class MoreCubit extends Cubit<MoreState> {
   MoreCubit() : super(MoreState.init());
 
-  void toggleNotifications(bool value) {
-    emit(state.copyWith(notificationsEnabled: value));
-  }
-
-  void toggleDarkMode(bool value) {
-    emit(state.copyWith(isDarkMode: value));
-  }
-
   void changeLanguage(String language) {
     emit(state.copyWith(selectedLanguage: language));
   }
