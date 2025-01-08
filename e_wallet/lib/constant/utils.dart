a3a6b1fcb4a8dbbf921b91bae7e9770ea77142b4
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 13:23:34 +0700

    Update utils.dart

--- e_wallet/lib/constant/utils.dart
+++ e_wallet/lib/constant/utils.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 String formattedPhone(String phone) {
   final cleanPhone = phone.replaceAll(RegExp(r'[^0-9]'), '');
@@ -97,4 +98,10 @@ IconData getTransactionIcon(String type) {
     default:
       return Icons.receipt_long;
   }
+}
+
+String formatCustomDate(BuildContext context, String dateString) {
+  final date = DateTime.parse(dateString);
+  final locale = Localizations.localeOf(context).languageCode;
+  return DateFormat('MMMM dd, yyyy - hh:mm a', locale).format(date);
 }
