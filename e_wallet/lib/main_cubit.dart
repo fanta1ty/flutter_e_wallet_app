620ba00052a165e9ebeba86cdbf3b63c381e8a40
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 08:33:47 +0700

    Update main_cubit.dart

--- e_wallet/lib/main_cubit.dart
+++ e_wallet/lib/main_cubit.dart
@@ -1,7 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 
 part 'main_state.dart';
 
 class MainCubit extends Cubit<MainState> {
-  MainCubit() : super(MainState.name());
+  MainCubit()
+      : super(
+          MainState.init(
+            themeData: _lightTheme,
+            isDarkMode: false,
+          ),
+        );
+
+  static final ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.deepPurple,
+    brightness: Brightness.light,
+  );
+
+  static final ThemeData _darkTheme = ThemeData(
+    primarySwatch: Colors.deepPurple,
+    brightness: Brightness.dark,
+  );
+
+  void toggleTheme(bool isDarkMode) {
+    emit(MainState(
+      themeData: isDarkMode ? _darkTheme : _lightTheme,
+      isDarkMode: isDarkMode,
+    ));
+  }
 }
