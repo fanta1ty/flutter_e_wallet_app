6676703b0b1cc3c1b13e93e8f4d94737c217a896
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 13:23:43 +0700

    Create app_localizations_vi.dart

---
+++ e_wallet/lib/l10n/app_localizations_vi.dart
@@ -0,0 +1,227 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get from => 'Từ';
+
+  @override
+  String get share_transaction => 'Chia sẻ giao dịch';
+
+  @override
+  String get no_note => 'Không có ghi chú';
+
+  @override
+  String get note => 'Ghi chú';
+
+  @override
+  String get transaction_detail => 'Chi tiết giao dịch';
+
+  @override
+  String get shared => 'Đã chia sẻ';
+
+  @override
+  String get amount => 'Số tiền';
+
+  @override
+  String get transaction_details => 'Chi tiết giao dịch';
+
+  @override
+  String get invalid_date => 'Ngày không hợp lệ';
+
+  @override
+  String get others => 'Khác';
+
+  @override
+  String get transaction_history => 'Lịch sử giao dịch';
+
+  @override
+  String get no_transactions_yet => 'Chưa có giao dịch nào';
+
+  @override
+  String get points => 'Điểm';
+
+  @override
+  String get profile => 'Hồ sơ';
+
+  @override
+  String get history => 'Lịch sử';
+
+  @override
+  String get home => 'Trang chủ';
+
+  @override
+  String get in_developing => 'Đang phát triển';
+
+  @override
+  String get transaction_overview => 'Tổng quan giao dịch';
+
+  @override
+  String get withdrawals => 'Rút tiền';
+
+  @override
+  String get transfers => 'Chuyển khoản';
+
+  @override
+  String get top_ups => 'Nạp tiền';
+
+  @override
+  String get more => 'Thêm';
+
+  @override
+  String get fee => 'Phí';
+
+  @override
+  String get back_to_home => 'Quay lại trang chủ';
+
+  @override
+  String get share => 'Chia sẻ';
+
+  @override
+  String get total_payment => 'Tổng thanh toán';
+
+  @override
+  String get reference_number => 'Số tham chiếu';
+
+  @override
+  String get time => 'Thời gian';
+
+  @override
+  String get date => 'Ngày';
+
+  @override
+  String get payment => 'Thanh toán';
+
+  @override
+  String get your_transaction_was_successful => 'Giao dịch của bạn đã thành công.';
+
+  @override
+  String get transfer_successful => 'Chuyển khoản thành công';
+
+  @override
+  String get transfer_to_banks => 'Chuyển đến ngân hàng';
+
+  @override
+  String get search_bank_code => 'Tìm mã ngân hàng';
+
+  @override
+  String get all_banks => 'Tất cả ngân hàng';
+
+  @override
+  String get recipient_name => 'Tên người nhận';
+
+  @override
+  String get to => 'Đến';
+
+  @override
+  String get select_bank_code => 'Chọn mã ngân hàng';
+
+  @override
+  String get bank_code => 'Mã ngân hàng';
+
+  @override
+  String get transfer_to_bank => 'Chuyển tiền đến ngân hàng';
+
+  @override
+  String get transfer => 'Chuyển khoản';
+
+  @override
+  String get to_friends => 'Đến bạn bè';
+
+  @override
+  String get to_bank => 'Đến ngân hàng';
+
+  @override
+  String get latest_transfer => 'Chuyển khoản gần đây';
+
+  @override
+  String get transfer_to_friends => 'Chuyển đến bạn bè';
+
+  @override
+  String get your_balance => 'Số dư của bạn';
+
+  @override
+  String get top_up => 'Nạp tiền';
+
+  @override
+  String get phone_number => 'Số điện thoại';
+
+  @override
+  String get input_phone_number => 'Nhập số điện thoại';
+
+  @override
+  String get notes_optional => 'Ghi chú (Tùy chọn)';
+
+  @override
+  String get write_your_notes_here => 'Viết ghi chú tại đây';
+
+  @override
+  String get set_amount => 'Nhập số tiền';
+
+  @override
+  String get proceed_to_transfer => 'Tiến hành chuyển khoản';
+
+  @override
+  String get top_up_amount => 'Số tiền nạp';
+
+  @override
+  String get bank_transfer => 'Chuyển khoản ngân hàng';
+
+  @override
+  String get top_up_now => 'Nạp tiền ngay';
+
+  @override
+  String get recent_top_ups => 'Các lần nạp gần đây';
+
+  @override
+  String get no_top_ups_yet => 'Chưa có lần nạp nào.';
+
+  @override
+  String get top_up_successful => 'Nạp tiền thành công!';
+
+  @override
+  String get has_been_added_to_your_balance => 'đã được cộng vào số dư của bạn.';
+
+  @override
+  String get return_to_home => 'Quay về trang chủ';
+
+  @override
+  String get withdraw => 'Rút tiền';
+
+  @override
+  String get withdrawal_amount => 'Số tiền rút';
+
+  @override
+  String get enter_custom_amount => 'Nhập số tiền tùy chỉnh';
+
+  @override
+  String get withdraw_now => 'Rút ngay';
+
+  @override
+  String get recent_withdrawals => 'Các lần rút gần đây';
+
+  @override
+  String get no_withdrawals_yet => 'Chưa có lần rút nào.';
+
+  @override
+  String get withdrawal_successful => 'Rút tiền thành công!';
+
+  @override
+  String get has_been_withdrawn => 'đã được rút.';
+
+  @override
+  String get settings => 'Cài đặt';
+
+  @override
+  String get language => 'Ngôn ngữ';
+
+  @override
+  String get select_language => 'Chọn ngôn ngữ';
+
+  @override
+  String get dark_mode => 'Chế độ tối';
+}
