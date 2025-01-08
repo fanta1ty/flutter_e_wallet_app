0ec406b6d766570c240f6da0b72fa2e4eee0c3c0
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 13:23:42 +0700

    Create app_localizations_en.dart

---
+++ e_wallet/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,227 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get from => 'From';
+
+  @override
+  String get share_transaction => 'Share Transaction';
+
+  @override
+  String get no_note => 'No Note';
+
+  @override
+  String get note => 'Note';
+
+  @override
+  String get transaction_detail => 'Transaction Detail';
+
+  @override
+  String get shared => 'Shared';
+
+  @override
+  String get amount => 'Amount';
+
+  @override
+  String get transaction_details => 'Transaction Details';
+
+  @override
+  String get invalid_date => 'Invalid Date';
+
+  @override
+  String get others => 'Others';
+
+  @override
+  String get transaction_history => 'Transaction History';
+
+  @override
+  String get no_transactions_yet => 'No transactions yet';
+
+  @override
+  String get points => 'Points';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get history => 'History';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get in_developing => 'Under Development';
+
+  @override
+  String get transaction_overview => 'Transaction Overview';
+
+  @override
+  String get withdrawals => 'Withdrawals';
+
+  @override
+  String get transfers => 'Transfers';
+
+  @override
+  String get top_ups => 'Top-Ups';
+
+  @override
+  String get more => 'More';
+
+  @override
+  String get fee => 'Fee';
+
+  @override
+  String get back_to_home => 'Back to Home';
+
+  @override
+  String get share => 'Share';
+
+  @override
+  String get total_payment => 'Total Payment';
+
+  @override
+  String get reference_number => 'Reference Number';
+
+  @override
+  String get time => 'Time';
+
+  @override
+  String get date => 'Date';
+
+  @override
+  String get payment => 'Payment';
+
+  @override
+  String get your_transaction_was_successful => 'Your transaction was successful.';
+
+  @override
+  String get transfer_successful => 'Transfer Successful';
+
+  @override
+  String get transfer_to_banks => 'Transfer to Banks';
+
+  @override
+  String get search_bank_code => 'Search Bank Code';
+
+  @override
+  String get all_banks => 'All Banks';
+
+  @override
+  String get recipient_name => 'Recipient Name';
+
+  @override
+  String get to => 'To';
+
+  @override
+  String get select_bank_code => 'Select Bank Code';
+
+  @override
+  String get bank_code => 'Bank Code';
+
+  @override
+  String get transfer_to_bank => 'Transfer to Bank';
+
+  @override
+  String get transfer => 'Transfer';
+
+  @override
+  String get to_friends => 'To Friends';
+
+  @override
+  String get to_bank => 'To Bank';
+
+  @override
+  String get latest_transfer => 'Latest Transfer';
+
+  @override
+  String get transfer_to_friends => 'Transfer to Friends';
+
+  @override
+  String get your_balance => 'Your Balance';
+
+  @override
+  String get top_up => 'Top Up';
+
+  @override
+  String get phone_number => 'Phone Number';
+
+  @override
+  String get input_phone_number => 'Enter Phone Number';
+
+  @override
+  String get notes_optional => 'Notes (Optional)';
+
+  @override
+  String get write_your_notes_here => 'Write your notes here';
+
+  @override
+  String get set_amount => 'Set Amount';
+
+  @override
+  String get proceed_to_transfer => 'Proceed to Transfer';
+
+  @override
+  String get top_up_amount => 'Top-Up Amount';
+
+  @override
+  String get bank_transfer => 'Bank Transfer';
+
+  @override
+  String get top_up_now => 'Top-Up Now';
+
+  @override
+  String get recent_top_ups => 'Recent Top-Ups';
+
+  @override
+  String get no_top_ups_yet => 'No top-ups yet.';
+
+  @override
+  String get top_up_successful => 'Top-Up Successful!';
+
+  @override
+  String get has_been_added_to_your_balance => 'has been added to your balance.';
+
+  @override
+  String get return_to_home => 'Return to Home';
+
+  @override
+  String get withdraw => 'Withdraw';
+
+  @override
+  String get withdrawal_amount => 'Withdrawal Amount';
+
+  @override
+  String get enter_custom_amount => 'Enter Custom Amount';
+
+  @override
+  String get withdraw_now => 'Withdraw Now';
+
+  @override
+  String get recent_withdrawals => 'Recent Withdrawals';
+
+  @override
+  String get no_withdrawals_yet => 'No withdrawals yet.';
+
+  @override
+  String get withdrawal_successful => 'Withdrawal Successful!';
+
+  @override
+  String get has_been_withdrawn => 'has been withdrawn.';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get select_language => 'Select Language';
+
+  @override
+  String get dark_mode => 'Dark Mode';
+}
