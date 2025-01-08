ec32feda26d00decfdc4794a1e17a4d88dae0782
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-01-08 08:44:33 +0700

    Update main_state.dart

--- e_wallet/lib/main_state.dart
+++ e_wallet/lib/main_state.dart
@@ -1,34 +1,63 @@
 part of 'main_cubit.dart';
 
+enum AppTheme { light, dark }
+
 class MainState {
-  MainState.name();
+  final ThemeData themeData;
+  final bool isDarkMode;
+
+  MainState.init({
+    required this.themeData,
+    required this.isDarkMode,
+  });
 
 //<editor-fold desc="Data Methods">
-  const MainState();
+  const MainState({
+    required this.themeData,
+    required this.isDarkMode,
+  });
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is MainState && runtimeType == other.runtimeType);
+      (other is MainState &&
+          runtimeType == other.runtimeType &&
+          themeData == other.themeData &&
+          isDarkMode == other.isDarkMode);
 
   @override
-  int get hashCode => 0;
+  int get hashCode => themeData.hashCode ^ isDarkMode.hashCode;
 
   @override
   String toString() {
-    return 'MainState{' + '}';
+    return 'MainState{' +
+        ' themeData: $themeData,' +
+        ' isDarkMode: $isDarkMode,' +
+        '}';
   }
 
-  MainState copyWith() {
-    return MainState();
+  MainState copyWith({
+    ThemeData? themeData,
+    bool? isDarkMode,
+  }) {
+    return MainState(
+      themeData: themeData ?? this.themeData,
+      isDarkMode: isDarkMode ?? this.isDarkMode,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    return {};
+    return {
+      'themeData': this.themeData,
+      'isDarkMode': this.isDarkMode,
+    };
   }
 
   factory MainState.fromMap(Map<String, dynamic> map) {
-    return MainState();
+    return MainState(
+      themeData: map['themeData'] as ThemeData,
+      isDarkMode: map['isDarkMode'] as bool,
+    );
   }
 
 //</editor-fold>
